8eef63ce37da283286b32a1b54315688e2a59c60
jesusmedrandam <118213578+jesusmedrandam@users.noreply.github.com>
2025-12-11 11:56:02 -0500

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,7 @@
+self.addEventListener("install", (e) => {
+    console.log("Service Worker instalado");
+});
+
+self.addEventListener("activate", (e) => {
+    console.log("Service Worker activo");
+});
